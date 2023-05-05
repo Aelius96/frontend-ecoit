b57eb6069b4482e0dcb0ae7e4d080004092af632
Aelius96 <nhat.tt.dev@gmail.com>
2023-05-05 11:09:05 +0700

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,20 @@ import {RouterModule, Routes} from "@angular/router";
 
 import {NgModule} from "@angular/core";
 import {HomeMainComponent} from "./modules/home/home-main/home-main.component";
+import {NewsListComponent} from "./modules/news/news-list/news-list.component";
+import {RecruitListComponent} from "./modules/recruit/recruit-list/recruit-list.component";
+import {PostListComponent} from "./modules/post/post-list/post-list.component";
+import {BlogListComponent} from "./modules/blog/blog-list/blog-list.component";
 
 const routes: Routes =[
   {
     path:'',redirectTo:'/trang-chu',pathMatch:'full',
   },
-  {path:'trang-chu', component: HomeMainComponent}
+  {path:'trang-chu', component: HomeMainComponent},
+  {path:'news',component:NewsListComponent},
+  {path:'recruit',component: RecruitListComponent},
+  {path:'post', component: PostListComponent},
+  {path:'blog', component: BlogListComponent}
 ]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import {AppRoutingModule} from "./app-routing.module";
 import {FormsModule} from "@angular/forms";
 
 import { HomeMainComponent } from './modules/home/home-main/home-main.component';
+import {BlogListComponent} from "./modules/blog/blog-list/blog-list.component";
+import {NewsListComponent} from "./modules/news/news-list/news-list.component";
+import {RecruitListComponent} from "./modules/recruit/recruit-list/recruit-list.component";
+import {PostListComponent} from "./modules/post/post-list/post-list.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeMainComponent,
+    BlogListComponent,
+    NewsListComponent,
+    RecruitListComponent,
+    PostListComponent
 
   ],
   imports: [
