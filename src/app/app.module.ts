6daf93945ebdfb45f72fcd6be54b75292d34e62e
Aelius96 <nhat.tt.dev@gmail.com>
2023-05-08 12:05:15 +0700

    fix app-routing and app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,16 +17,15 @@ import { AboutUsComponent } from './modules/introduce/about-us/about-us.componen
 import { CapacityProfileComponent } from './modules/introduce/capacity-profile/capacity-profile.component';
 import { TypicalCustomersComponent } from './modules/introduce/typical-customers/typical-customers.component';
 import { ContactComponent } from './modules/contact/contact.component';
-<<<<<<< HEAD
+
 import { ErrorComponent } from './modules/error/error.component';
-=======
+
 import {BlogListComponent} from "./modules/blog/blog-list/blog-list.component";
 import {NewsListComponent} from "./modules/news/news-list/news-list.component";
 import {RecruitListComponent} from "./modules/recruit/recruit-list/recruit-list.component";
 import {PostListComponent} from "./modules/post/post-list/post-list.component";
 import { SideBarComponent } from './modules/navigation/side-bar/side-bar.component';
 
->>>>>>> 6124478cad044a628b3a08391ee8c0c31cc9f4e1
 
 @NgModule({
   declarations: [
@@ -42,16 +41,12 @@ import { SideBarComponent } from './modules/navigation/side-bar/side-bar.compone
     CapacityProfileComponent,
     TypicalCustomersComponent,
     ContactComponent,
-<<<<<<< HEAD
     ErrorComponent,
-=======
     BlogListComponent,
     NewsListComponent,
     RecruitListComponent,
     PostListComponent,
     SideBarComponent
-
->>>>>>> 6124478cad044a628b3a08391ee8c0c31cc9f4e1
 
   ],
   imports: [
