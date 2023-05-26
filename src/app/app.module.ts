9c11105edda3100c382fc405c92c68ab2405c0e8
@vdb5421 <vubaduong.2001@gmail.com>
2023-05-26 08:36:24 +0700

    updatee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -55,6 +55,10 @@ import { CusotmerControlComponent } from './modules/customer/cusotmer-control/cu
 import { GalleryControlComponent } from './modules/typical/gallery/gallery-control/gallery-control.component';
 import { GalleryAddComponent } from './modules/typical/gallery/gallery-add/gallery-add.component';
 import { CustomerAddComponent } from './modules/customer/customer-add/customer-add.component';
+import { UserControlComponent } from './modules/user/user-control/user-control.component';
+import { UserAddComponent } from './modules/user/user-add/user-add.component';
+import { UserActiveComponent } from './modules/user/user-active/user-active.component';
+import { HistoryListComponent } from './modules/admin/history/history-list/history-list.component';
 
 
 @NgModule({
@@ -105,6 +109,10 @@ import { CustomerAddComponent } from './modules/customer/customer-add/customer-a
     GalleryControlComponent,
     GalleryAddComponent,
     CustomerAddComponent,
+    UserControlComponent,
+    UserAddComponent,
+    UserActiveComponent,
+    HistoryListComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/modules/_container/admin.module.ts
+++ src/app/modules/_container/admin.module.ts
@@ -3,6 +3,8 @@ import {CommonModule} from "@angular/common";
 import {RouterModule, Routes} from "@angular/router";
 import {DashboardComponent} from "../admin/dashboard/dashboard.component";
 import {AdminComponent} from "../admin/admin/admin.component";
+import { UserControlComponent } from "../user/user-control/user-control.component";
+import { HistoryListComponent } from "../admin/history/history-list/history-list.component";
 
 
 const routes: Routes =[
@@ -11,13 +13,14 @@ const routes: Routes =[
 
   children:[
     {path: 'dashboard' , component: DashboardComponent},
-    {path: 'post' , title:'admin-tin tức', loadChildren:()=> import('./post.module').then(m=>m.PostModule)},
-    {path: 'blogs' , title:'admin-blog', loadChildren:()=> import('./blog.module').then(m=>m.BlogModule)},
-    {path: 'news' , title:'admin-tin tức', loadChildren:()=> import('./news.modules').then(m=>m.NewsModules)},
-    {path: 'recuit' , title:'admin-tuyển dụng', loadChildren:()=> import('./recuit.module').then(m=>m.RecuitModule)},
-    {path: 'customer' , title:'admin-khách hàng' , loadChildren:()=>import('./customer.module').then(m=>m.CustomerModule)},
-    {path: 'image-gallery', title:'admin-khách hàng tiêu biểu', loadChildren:()=>import('./gallery.module').then(m=>m.GalleryModule) },
-
+    {path: 'post' , title:'Admin-tin tức', loadChildren:()=> import('./post.module').then(m=>m.PostModule)},
+    {path: 'blogs' , title:'Admin-blog', loadChildren:()=> import('./blog.module').then(m=>m.BlogModule)},
+    {path: 'news' , title:'Admin-tin tức', loadChildren:()=> import('./news.modules').then(m=>m.NewsModules)},
+    {path: 'recuit' , title:'Admin-tuyển dụng', loadChildren:()=> import('./recuit.module').then(m=>m.RecuitModule)},
+    {path: 'customer' , title:'Admin-khách hàng' , loadChildren:()=>import('./customer.module').then(m=>m.CustomerModule)},
+    {path: 'image-gallery', title:'Admin-khách hàng tiêu biểu', loadChildren:()=>import('./gallery.module').then(m=>m.GalleryModule) },
+    {path: 'user', title:'Admin-tài khoản', component:UserControlComponent },
+    {path:'history' , title: 'Admin-lịch sử sử đổi' , component: HistoryListComponent},
   ]},
 ]
 
